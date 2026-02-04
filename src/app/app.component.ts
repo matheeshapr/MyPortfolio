310e465fb735910166ba67b403358ebfb905307f
matheeshapr <matheeshap62@gmail.com>
2026-02-04 11:27:29 +0530

    feat: refactor app component imports and structure for improved clarity and organization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { HeroComponent } from "./components/hero/hero.component";
-import { ProjectsComponent } from "./components/projects/projects.component";
-import { ContactComponent } from "./components/contact/contact.component";
-import { AboutComponent } from "./components/about/about.component";
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+// අවශ්‍ය components import කර ඇති බව සහතික කරගන්න
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, ProjectsComponent, ContactComponent, AboutComponent],
+  standalone: true, // ඔබ standalone components පාවිච්චි කරන්නේ නම් මෙය අවශ්‍යයි
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent implements OnInit {
-  title = 'my-portfolio';
-
   ngOnInit() {
-  AOS.init({
-    duration: 1000,
-    once: false,   
-    mirror: true,  
-    offset: 100    
-  });
-}
+    AOS.init({
+      duration: 1000,
+      once: false,
+      mirror: true
+    });
+  }
 }
